b1b5fc627c1edd2afffb3c99ced5e8ec88248099
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2023-10-17 18:39:50 -0700

    Add files via upload

---
+++ EVA2_8_ELSE_IF/src/eva2_8_else_if/EVA2_8_ELSE_IF.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_8_else_if;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_8_ELSE_IF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int mes;
+       System.out.println("introduce numero del mes");
+       mes = input.nextInt();
+       //IF((MES>= 1) && (MES<= 12))
+       if((mes >= 1) && (mes <= 12))
+           if(mes == 1);
+               System.out.println("ENERO");
+           if(mes == 2);
+               System.out.println("FEBRERO");
+           if(mes == 3);
+               System.out.println("MARZO");
+           if(mes == 4);
+               System.out.println("ABRIL");
+           if(mes == 5);
+               System.out.println("MAYO");
+           if(mes == 6);
+               System.out.println("JUNIO");
+           if(mes == 7);
+               System.out.println("JULIO");
+           if(mes == 8);
+               System.out.println("AGOSTO");
+           if(mes == 9);
+               System.out.println("SEPTEMBRE");
+           if(mes == 10);
+               System.out.println("OCTUBRE");
+           if(mes == 11);
+               System.out.println("NOVIEMBRE");
+           if(mes == 12);
+               System.out.println("DICIEMBRE");
+           //
+           
+    }
+    
+}
