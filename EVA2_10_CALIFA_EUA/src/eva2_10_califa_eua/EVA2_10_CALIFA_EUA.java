7e47d03c330c81d0135058be96a50a1a372fddd8
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2023-10-18 18:44:38 -0700

    Add files via upload

---
+++ EVA2_10_CALIFA_EUA/src/eva2_10_califa_eua/EVA2_10_CALIFA_EUA.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_10_califa_eua;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_10_CALIFA_EUA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int califa;
+       System.out.println("introduce una califa");
+       califa = input.nextInt();
+       if((califa >= 60) && (califa <= 100))
+           if(califa == 100);
+               System.out.println("A");
+           if(califa == 89);
+               System.out.println("B");
+           if(califa == 79);
+               System.out.println("C");
+           if(califa == 69);
+               System.out.println("D");
+           if(califa <= 60);
+               System.out.println("F");
+           
+    }
+    
+}
